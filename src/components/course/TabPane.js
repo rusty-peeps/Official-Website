bceca88e1a9397d9e09f6944f3278a07d1ab6e30
NishikantaRay <nishikantaray1@gmail.com>
2024-11-28 21:37:30 +0530

    added

--- src/components/course/TabPane.js
+++ src/components/course/TabPane.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import CoursesList from './CoursesList';
 import courses from '../../data/courseList.json';
+import courses1 from '../../data/courseList1.json';
+import courses2 from '../../data/courseList2.json';
+import courses3 from '../../data/courseList3.json';
+import courses4 from '../../data/courseList4.json';
 const TabPane = ({ id, label, isActive }) => {
   return (
+    <>
     <div
       className={`tab-pane fade ${isActive ? 'show active' : ''}`}
       id='pills-home'
@@ -14,6 +19,53 @@ const TabPane = ({ id, label, isActive }) => {
         <CoursesList courses={courses}/>
       </div>
     </div>
+    <div
+      className={`tab-pane fade ${isActive ? 'show active' : ''}`}
+      id='pills-profile'
+      role="tabpanel"
+      aria-labelledby={`${id}-tab`}
+      tabIndex="0"
+    >
+      <div className="row">
+        <CoursesList courses={courses1}/>
+      </div>
+    </div>
+    <div
+      className={`tab-pane fade ${isActive ? 'show active' : ''}`}
+      id='pills-contact'
+      role="tabpanel"
+      aria-labelledby={`${id}-tab`}
+      tabIndex="0"
+    >
+      <div className="row">
+        <CoursesList courses={courses2}/>
+      </div>
+    </div>
+    <div
+      className={`tab-pane fade ${isActive ? 'show active' : ''}`}
+      id='pills-four'
+      role="tabpanel"
+      aria-labelledby={`${id}-tab`}
+      tabIndex="0"
+    >
+      <div className="row">
+        <CoursesList courses={courses3}/>
+      </div>
+    </div>
+    <div
+      className={`tab-pane fade ${isActive ? 'show active' : ''}`}
+      id='pills-five'
+      role="tabpanel"
+      aria-labelledby={`${id}-tab`}
+      tabIndex="0"
+    >
+      <div className="row">
+        <CoursesList courses={courses4}/>
+      </div>
+    </div>
+    </>
+    
+    
   );
 };
 
