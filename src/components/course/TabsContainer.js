c199cfefe23c6187ed82e22ad0b12de42dfa7c18
NishikantaRay <nishikantaray1@gmail.com>
2024-11-28 21:43:26 +0530

    added

--- src/components/course/TabsContainer.js
+++ src/components/course/TabsContainer.js
@@ -15,14 +15,20 @@ const TabsContainer = () => {
   return (
     <div>
       <div className="tab-content" id="pills-tabContent">
-        {tabs.map((tab) => (
+      <TabPane
+            // key={tab.id}
+            // id={tab.id}
+            // label={tab.label}
+            // isActive={activeTab === tab.id}
+          />
+        {/* {tabs.map((tab) => (
           <TabPane
             key={tab.id}
             id={tab.id}
             label={tab.label}
             isActive={activeTab === tab.id}
           />
-        ))}
+        ))} */}
       </div>
     </div>
   );
